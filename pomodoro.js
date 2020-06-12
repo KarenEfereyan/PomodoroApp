c1708480c231fd1ec6d889f775e5a40ecbd102fd
Karen Efereyan <thedevkaren@gmail.com>
2020-06-12 02:27:38 +0100

    Select Elements from DOM

---
+++ pomodoro.js
@@ -0,0 +1,39 @@
+//SELECT THE ELEMENTS FROM THE DOM THAT ARE NEEDED IN THIS SCRIPT
+const pomoTimer = document.querySelector("#pomodoro-timer");
+const startClock = document.querySelector("#pomodoro-start");
+const pauseClock = document.querySelector("#pomodoro-pause");
+const stopClock = document.querySelector("#pomodoro-stop");
+
+//Variable Declarations
+
+//We need to know if the clock is already running before deciding what to do
+let isClockRunning = false;
+
+//Work sessionTime
+let workSession = 1500; //25mins
+let timeLeftInWorkSession = 1500; //25mins, this reduces as the clock counts down
+let breakSession = 300; //5mins, it can be changed as needed
+
+// Attach event listeners to all three buttons
+// START
+startClock.addEventListener("click", () => {
+  alert("Hey! I start the clock");
+  //toggleClock();
+});
+
+// PAUSE
+pauseClock.addEventListener("click", () => {
+  alert("Hey! I pause the clock");
+  //toggleClock();
+});
+
+// STOP
+stopClock.addEventListener("click", () => {
+  alert("I stop the clock completely");
+  //toggleClock(true);
+});
+
+//Pomodoro App Functions
+function whatShouldIDo() {
+  //Defines what should be done when each button is clicked
+}
